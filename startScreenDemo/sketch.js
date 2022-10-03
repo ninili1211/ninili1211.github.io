2d63b84e1356053c0031b474f147b12d9663d2e9
Li1302345 <1302345@sps-student.sk.ca>
2022-10-03 12:17:04 -0600

    coded demo

--- startScreenDemo/sketch.js
+++ startScreenDemo/sketch.js
@@ -1,10 +1,13 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Start Screen Demo
+// Nini Li
+// 10/03/2022
 
+let state = "start";
+let treesImg;
+
+function preload() {
+  treesImg = loadImage("foodpic/jpg")
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +15,30 @@ function setup() {
 
 function draw() {
   background(220);
+  if (state === "start") {
+    startScreen();
+    if (state === "main") {
+      image(foodPic, 0, 0);
+    }
+  }
+}
+
+function mousePressed() {
+  if (state === "start" && mouseInsideRect(400, 700, 400, 550)) {
+    state = "main";
+  }
+}
+
+function startScreen() {
+  if (mouseInsideRect(400, 700, 400, 550)) {
+    fill ("gray");
+  }
+  else {
+  fill("black");
+  rect(400, 400, 300, 150);
+}
+
+function mouseInsideRect(left, right, top, bottom) {
+  return mouseX >=  left && mouseX <= right && 
+  mouseY >= top && mouseY <= bottom;
 }
