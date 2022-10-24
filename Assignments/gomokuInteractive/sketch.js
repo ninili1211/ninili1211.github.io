1eca7ce13dab4b18e6bac9d172346187cd5da075
ninili1211 <107947384+ninili1211@users.noreply.github.com>
2022-10-23 19:20:01 -0600

    update

--- Assignments/gomokuInteractive/sketch.js
+++ Assignments/gomokuInteractive/sketch.js
@@ -1,15 +1,49 @@
 // Gomoku Interactive
 // Nini Li
-// Date
+// 09/20/22
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - 
 
+//variables
+let state = "black";
+let scalar = 7;
+let x, y, cellWidth, cellHeight, circleX, circleY, coordinateX, coordinateY;
+let woodImage;
+
+//setup + draw
+function preload() {
+  woodImage = loadImage("wood.jpg");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
 }
 
 function draw() {
   background(220);
+  windowResized();
+  chessboard();
+  image(woodImage, 0, 0, woodImage.width * scalar);
+}
+
+function windowResized() {
+  setup();
+}
+
+function chessboard() {
+  
+}
+
+function coordinates() {
+
+}
+
+function blackDot() {
+  
+}
+
+function whiteDot() {
+
 }
