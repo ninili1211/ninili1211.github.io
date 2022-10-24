f328110e3ed1218b702d5dbbde2fdc67374c514f
ninili1211 <107947384+ninili1211@users.noreply.github.com>
2022-10-23 19:21:23 -0600

    update

--- Assignments/gomokuInteractive/sketch.js
+++ Assignments/gomokuInteractive/sketch.js
@@ -1,15 +1,15 @@
-// Gomoku Interactive
-// Nini Li
-// 09/20/22
-//
-// Extra for Experts:
-// - 
+//Interactive Assignment - Gomoku 
+// Li
+//Sept 20, 2022
+//Extra For Experts - Centered Circle that's not an image. coded algorithm
 
-//variables
-let state = "black";
-let scalar = 7;
-let x, y, cellWidth, cellHeight, circleX, circleY, coordinateX, coordinateY;
+//global variables
+let colourstate = "black";
+let x = 400;
+let y = 400;
+let cellWidth, cellHeight, circleX, circleY, coordinateX, coordinateY;
 let woodImage;
+let scalar = 7;
 
 //setup + draw
 function preload() {
@@ -22,28 +22,73 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-  windowResized();
-  chessboard();
-  image(woodImage, 0, 0, woodImage.width * scalar);
+  background(255, 232, 214);
+  image(woodImage, 0, 0, woodImage.width*scalar, woodImage.height*scalar);
+  drawChessboard();
+  drawCoordinates();
 }
 
 function windowResized() {
   setup();
 }
 
-function chessboard() {
-  
+//chessboard
+function drawChessboard() {
+  cellWidth = width*0.75/18;
+  cellHeight = height/18;
+  if (cellWidth > cellHeight) {
+    cellWidth = cellHeight;
+  }
+  else {
+    cellHeight = cellWidth;
+  }
+  push();
+  translate(width/4, cellHeight);
+  for (let y = 0; y < 18; y++) {
+    for (let x = 0; x < 18; x++) {
+      stroke("gray");
+      fill(255, 232, 214);
+      rect(x*cellWidth-25, y*cellHeight-25, cellWidth, cellHeight);
+    }
+  }
+  pop();
+  if (colourstate === "black") {
+    blackDot();
+  }
 }
 
-function coordinates() {
-
+//coordinates
+function drawCoordinates() {
+  cellWidth = width*0.75/18;
+  cellHeight = height/19;
+  coordinateX = 0;
+  coordinateY = 0;
+  if (cellWidth > cellHeight) {
+    cellWidth = cellHeight;
+  }
+  else {
+    cellHeight = cellWidth;
+  }
+  for (let i = 0; i < 18; i += cellWidth) {
+    for (let j = 0; j < 18; j += cellHeight) {
+      ellipse(i*cellWidth-25, j*cellHeight-25, 10, 10);
+      console.log(i*cellWidth);
+      console.log(j*cellHeight);
+    }
+  }
 }
 
+//the pieces
 function blackDot() {
-  
+  circleX = cellWidth;
+  circleY = cellHeight;
+  fill("black");
+  ellipse(mouseX, mouseY, circleX, circleY);
 }
 
 function whiteDot() {
-
+  circleX = cellWidth;
+  circleY = cellHeight;
+  fill("white");
+  ellipse(mouseX, mouseY, circleX, circleY);
 }
