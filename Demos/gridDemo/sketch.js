957933f1e645081f7196a1b99531265aff122d81
Li1302345 <1302345@sps-student.sk.ca>
2022-10-25 12:16:56 -0600

    demo

--- Demos/gridDemo/sketch.js
+++ Demos/gridDemo/sketch.js
@@ -2,9 +2,10 @@
 // Nini
 // 10/25/2022
 
-let grid = [[0, 0, 1],
-            [1, 0, 1],
-            [0, 1, 0]];
+let state = "first";
+let grid = [[1, 0, 1, 0, 1],
+  [1, 0, 1, 0, 1],
+  [0, 1, 0, 1, 0]];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -19,7 +20,7 @@ function displayGrid(grid) {
   let cellWidth = width/grid[0].length;
   let cellHeight = height/grid.length;
   for (let y = 0; y < grid.length; y++) {
-    for ( let x = 0; x < grid[y].length; x++) {
+    for (let x = 0; x < grid[y].length; x++) {
       if (grid[y][x] === 0) {
         fill("white");
       }
@@ -29,4 +30,18 @@ function displayGrid(grid) {
       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
   }
+}
+
+function mousePressed() {
+  console.log(mouseX, mouseY);
+  let cellWidth = width/grid[0].length;
+  let cellHeight = height/grid.length;
+  let x = Math.floor(mouseX/cellWidth);
+  let y = Math.floor(mouseY/cellHeight);
+  if (grid[y][x] === 0){
+    grid[y][x] = 1;
+  }
+  else if (grid[y][x] === 1){
+    grid[y][x] = 0;
+}
 }
