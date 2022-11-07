94891acc7bd5f8ae9da39b667e404b47296945ff
Li1302345 <1302345@sps-student.sk.ca>
2022-11-07 11:17:19 -0600

    finished

--- test/sketch.js
+++ test/sketch.js
@@ -1,16 +1,85 @@
-function draw() {
-  cellWidth = width/18;
-  cellHeight = height/18;
-  if (cellWidth > cellHeight) {
-    cellWidth = cellHeight;
+function make2DArray(cols, rows) {
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
   }
-  else {
-    cellHeight = cellWidth;
+  return arr;
+}
+
+let grid;
+let cols;
+let rows;
+let w = 20;
+
+let totalBees = 30;
+
+function setup() {
+  createCanvas(401, 401);
+  cols = floor(width / w);
+  rows = floor(height / w);
+  grid = make2DArray(cols, rows);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = new Cell(i, j, w);
+    }
   }
 
-  for (let y = 0; y < 8; y++) {
-    for (let x = 0; x < 8; x++) {
-      ellipse(x*cellWidth, y*cellHeight, 2, 2);
+  // Pick totalBees spots
+  let options = [];
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      options.push([i, j]);
+    }
+  }
+
+
+  for (let n = 0; n < totalBees; n++) {
+    let index = floor(random(options.length));
+    let choice = options[index];
+    let i = choice[0];
+    let j = choice[1];
+    // Deletes that spot so it's no longer an option
+    options.splice(index, 1);
+    grid[i][j].bee = true;
+  }
+
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j].countBees();
+    }
+  }
+
+}
+
+function gameOver() {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j].revealed = true;
+    }
+  }
+}
+
+function mousePressed() {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      if (grid[i][j].contains(mouseX, mouseY)) {
+        grid[i][j].reveal();
+
+        if (grid[i][j].bee) {
+          gameOver();
+        }
+
+      }
+    }
+  }
+}
+
+function draw() {
+  background(255);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j].show();
     }
   }
 }
