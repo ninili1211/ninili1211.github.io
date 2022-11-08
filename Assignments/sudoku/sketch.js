6e533d4e83edd5415de79ef657045f9d2b0ed8d1
Li1302345 <1302345@sps-student.sk.ca>
2022-11-08 11:31:23 -0600

    update

--- Assignments/sudoku/sketch.js
+++ Assignments/sudoku/sketch.js
@@ -1,63 +1,132 @@
-// Checkers
+// Sudoku
 // Nini
 // 10/28/2022
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-const ROWS = 9;
-const COLS = 9;
-let grid;
-let cellWidth;
-let cellHeight;
+//global variables
+let tileSize = 133; 
+let tileRows = []; 
 
+//setup
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  cellWidth = windowHeight/COLS;
-  cellHeight = windowHeight/ROWS;
-  grid = create2dArray(COLS, ROWS);
+  createCanvas(400, 400);
+  setupTiles();
 }
 
+function setupTiles() {
+  for (let row = 0; row < 9; row++) {
+    tileRows[row] = [];
+    for (let col = 0; col < 9; col++) {
+      tileRows[row][col] = new Tile(col, row);
+    }
+  }
+}
+
+//reset
+function keyPressed() {
+  resetGame();
+}
+
+function resetGame() {
+  createCanvas(400, 400);
+  setupTiles();
+}
+
+//draw
 function draw() {
-  background("white");
-  displayGrid(grid);
+  background(255, 232, 214);
+  drawGrid();
+  drawTiles();
 }
 
-function mousePressed() {
-  let xPos = Math.floor(mouseX/cellWidth);
-  let yPos = Math.floor(mouseY/cellHeight);
+//game board
+function drawGrid() {
+  noFill();
+  stroke(0);
+  for (let row = 0; row < 9; row++) {
+    line(0, row * tileSize, tileSize * 3, row * tileSize);
+  }
+  for (let col = 0; col < 9; col++) {
+    line(col * tileSize, 0, col * tileSize, tileSize * 3);
+  }
 
-  if (grid[yPos][xPos] === 0) {
-    grid[yPos][xPos] = 1;
-  }
-  else if (grid[yPos][xPos] === 1) {
-    grid[yPos][xPos] = 0;
-  }
 }
-
-function displayGrid(grid) {
-  let isGreen = true;
-  for (let y=0; y<ROWS; y++) {
-    for (let x=0; x<COLS; x++) {
-      if (isGreen === true) {
-        fill("#d8f3dc");
-      }
-      else {
-        fill("#a2d2ff");
-      }
-      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
-      isGreen = !isGreen;
+function drawTiles() { 
+  //tiles
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      tileRows[row][col].show();
     }
   }
 }
 
-function create2dArray(COLS, ROWS) {
-  let emptyArray = [];
-  for (let y=0; y<ROWS; y++) {
-    emptyArray.push([]);
-    for (let x=0; x<COLS; x++) {
-      emptyArray[y].push(0);
+//when mouse is clicked
+// function mouseClicked() {
+//   for (let row = 0; row < 9; row++) {
+//     for (let col = 0; col < 9; col++) {
+//       ifEmpty(tileRows[row][col]);
+//     }
+//   }
+// }
+
+// function ifEmpty(currentTile) { 
+//   //check if tile empty
+//   if (currentTile.isMouseInBounds() && currentTile.isEmpty()) {
+//     currentTile.change(currentPlayer);
+//     switchPlayer();
+//   }
+// }
+
+//making each tile an actual object
+class Tile {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.theTurn = " ";
+  }
+
+  isEmpty() {
+    if (this.theTurn === " ") {
+      //if empty
+      return true;
+    } 
+    else {
+      //if not then error
+      alert("Must place on empty tile");//found alert outside of p5js references
+      //thankful that the pop up actually works
+      return false;
     }
   }
-  return emptyArray;
+
+  //checking if mouse is inside the tile
+  isMouseInBounds() {
+    let dx = this.x * tileSize;
+    let dy = this.y * tileSize;
+    if (mouseX > dx &&
+      mouseX < dx + tileSize &&
+      mouseY > dy &&
+      mouseY < dy + tileSize) {
+      return true;
+    }
+    return false;
+  }
+
+  //show the tile
+  // show() {
+  //   let dx = this.x * tileSize;
+  //   let dy = this.y * tileSize;
+  //   if (this.isMouseInBounds()) {
+  //     fill(150);
+  //     rect(dx, dy, tileSize, tileSize);
+  //   }
+
+  //   //display text
+  //   fill(0);
+  //   push();
+  //   translate(tileSize/2, tileSize/2); //still struggling with centering the entire thing, so i just did the text alone
+  //   text(this.theTurn, dx, dy);
+  //   pop();
+  // }
 }
