304f6f769a67bed4b9a9380edefcf4df27a2ab73
Li1302345 <1302345@sps-student.sk.ca>
2022-11-09 12:06:07 -0600

    update

--- Assignments/sudoku/sketch.js
+++ Assignments/sudoku/sketch.js
@@ -7,6 +7,8 @@
 
 //global variables
 let tileSize = 133; 
+let ROWS = 9;
+let COLS = 9;
 
 let grid = [[0, 0, 7, 4, 9, 1, 6, 0, 5],
   [2, 0, 0, 0, 6, 0, 3, 0, 9],
@@ -22,73 +24,31 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-
 function draw() {
   background(220);
   displayGrid(grid);
 }
 
-function mousePressed() {
-  console.log(mouseX, mouseY);
-  let cellWidth = width / grid[0].length;
-  let cellHeight = height / grid.length;
-
-  let x = Math.floor(mouseX/cellWidth);
-  let y = Math.floor(mouseY/cellHeight);
-  if (grid[y][x] === 0) {
-    grid[y][x] = 0;
-  }
-  if (grid[y][x] === 1) {
-    grid[y][x] = 0;
-  }
-  
-}
-
 function displayGrid(grid) {
   let cellWidth = width / grid[0].length;
   let cellHeight = height / grid.length;
-  for (let y=0; y<grid.length; y++) {
-    for (let x=0; x<grid[y].length; x++) {
+  for (let y = 0; y<grid.length; y++) {
+    for (let x = 0; x<grid[y].length; x++) {
       if (grid[y][x] === 0) {
-        fill("#c6d2ed");
-      }
-      if (grid[y][x] === 1) {
         fill("#edf6f9");
-        text("1");
       }
-      if (grid[y][x] === 2) {
-        fill("#edf6f9");
-        text("2");
-      }
-      if (grid[y][x] === 3) {
-        fill("#edf6f9");
-        text("3");
-      }
-      if (grid[y][x] === 4) {
-        fill("#edf6f9");
-        text("4");
-      }
-      if (grid[y][x] === 5) {
-        fill("#edf6f9");
-        text("5");
-      }
-      if (grid[y][x] === 6) {
-        fill("#edf6f9");
-        text("6");
-      }
-      if (grid[y][x] === 7) {
-        fill("#edf6f9");
-        text("7");
-      }
-      if (grid[y][x] === 8) {
-        fill("#edf6f9");
-        text("8");
-      }
-      if (grid[y][x] === 9) {
-        fill("#edf6f9");
-        text("1");
+      else {
+        fill("#c8e7ff");
       }
       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
   }
 }
+
+for (let y < 0; y < grid.length; y++) {
+  fill("#cdb4db");
+  rect()
+}
+
+//cdb4db
+//c6d2ed
