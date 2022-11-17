a78e0b7f8c7679c95d656121a5ed8f08e30ad482
Li1302345 <1302345@sps-student.sk.ca>
2022-11-17 11:29:03 -0600

    demo update

--- Demos/oopBulletDemo/sketch.js
+++ Demos/oopBulletDemo/sketch.js
@@ -3,61 +3,49 @@
 // 11/16/22
 
 class Bullet {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.radius = random(25, 100);
+  constructor() {
+    this.x = 200;
+    this.y = 500;
+    this.radius = 3;
     this.dx = 5;
-    this.theColor = color(random(255), random(255), random(255), random(255));
   }
   
   move() {
     this.x += this.dx;
+  }
 
-    // //left-right edges
-    // if (this.x + this.radius > width ||
-    //   this.x - this.radius < 0) {
-    //   this.dx *= -1;
-    // }
-
-    // //top-bottom edges
-    // if (this.y + this.radius > height || 
-    //   this.y - this.radius < 0) {
-    //   this.dy *= -1;
-    // }
+  isDead() {
+    return this.x >= width;
   }
 
   display() {
-    let x = 50;
-    let y = 50;
-    stroke(this.color);
-    fill(this.color);
+    fill("black");
     circle(this.x, this.y, this.radius*2);
   }
 }
 
-let theBullet = [];
+let bullets = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  let someBullet = new Bullet(100, 100);
-  theBullet.push(someBullet);
 }
 
 function draw() {
   background(220);
-  for (let i = 0; i < theBullet.length; i++) {
-    theBullet[i].move();
-    for (let j = 0; j < theBullet.length; j++) {
-      if (i !== j) {
-        theBullet[i].collisionCheck(theBullet[j]);
-      }
+  for (let theBullet of bullets) {
+    theBullet.move();
+    theBullet.display();
+  }
+ 
+  //delete dead bullets
+  for (let i = bullets.length - 1; i >= 0; i--) {
+    if (bullets[i].isDead()) {
+      bullets.splice(i, 1);
     }
-    theBullet[i].display();
   }
 }
 
 function mousePressed() {
-  let someBullet = new Bullet(x, y);
-  theBullet.push(theBullet);
+  let theBullet = new Bullet();
+  bullets.push(theBullet);
 }
